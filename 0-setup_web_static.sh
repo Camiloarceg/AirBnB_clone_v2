8ecd1680eba521a29041092e3caff847b5c99492
Camiloarceg <camiloarceg@gmail.com>
2022-05-21 14:35:38 -0500

    add task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# script that sets up your web servers for the deployment of web_static.
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
+sudo service nginx restart

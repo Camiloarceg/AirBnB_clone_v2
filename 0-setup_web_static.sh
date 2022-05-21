3ad2973b3f991e437a884226e31acfe9ace738d5
Camiloarceg <camiloarceg@gmail.com>
2022-05-21 14:45:19 -0500

    add

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
-# script that sets up your web servers for the deployment of web_static.
-sudo apt-get -y update
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
+# Sets web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get install -y nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "<!DOCTYPE html>
+<head>
+</head>
+<body>
+    Holberton School
+</body>
+<html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
+f_config="/etc/nginx/sites-available/default"
+sed -i "/listen 80 default_server;/a location /hbnb_static/ {alias /data/web_static/current/;}" $f_config
 sudo service nginx restart

39686ecab1946327f4237a7f9d4edafb38fbca67
Camiloarceg <camiloarceg@gmail.com>
2022-05-22 07:10:13 -0500

    QA review corrections

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -24,21 +24,20 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """ Distributes an archive to your web servers. """
-    if not exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-    path = "/data/web_static/releases/"
-    name = archive_path.split('.')[0].split('/')[1]
-    dest = path + name
     try:
-        put(archive_path, '/tmp')
-        run('mkdir -p {}'.format(dest))
-        run('tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
-        run('rm -f /tmp/{}.tgz'.format(name))
-        run('mv {}/web_static/* {}/'.format(dest, dest))
-        run('rm -rf {}/web_static'.format(dest))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(dest))
+        file_name = archive_path.split("/")[-1]
+        file_not_ext = file_name.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run("mkdir -p {}{}".format(path, file_not_ext))
+        run("tar xzf /tmp/{} -C {}{}/".format(file_name, path, file_not_ext))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_not_ext))
+        run("rm /tmp/{}".format(file_name))
+        run("rm /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(path, file_not_ext))
         return True
     except Exception:
         return False
